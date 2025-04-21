777f2661caf72f3d50ea56239727b155439cc9ba
Stefano Mini <info@stefanominiconsulting.com>
2025-04-21 10:22:22 +0300

    Adds dog page with placeholders

---
+++ lib/edit_kennel/dog/tags.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class EditTagsDialog extends StatelessWidget {
+  const EditTagsDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Dialog(
+      child: Placeholder(),
+    );
+  }
+}
+
+class AddTagDialog extends StatelessWidget {
+  const AddTagDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Placeholder(),
+    );
+  }
+}
