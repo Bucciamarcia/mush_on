6e7c325eeb04accb8648e46332853200e3ceb458
Stefano Mini <info@stefanominiconsulting.com>
2025-04-30 12:07:59 +0300

    Init dog filter

--- lib/home_page/main.dart
+++ lib/home_page/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mush_on/services/auth.dart';
+import 'package:mush_on/shared/dog_filter/main.dart';
 
 class HomePageScreenContent extends StatelessWidget {
   const HomePageScreenContent({super.key});
@@ -45,6 +46,12 @@ class HomePageScreenContent extends StatelessWidget {
             },
             child: Text("Log out"),
           ),
+          ElevatedButton(
+              onPressed: () async {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DogFilterWidget()));
+              },
+              child: Text("Moi")),
         ],
       ),
     );
