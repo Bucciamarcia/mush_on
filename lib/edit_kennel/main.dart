a3fac38a46ac77fb9a35430487a8a377e6e6bdc4
Stefano Mini <info@stefanominiconsulting.com>
2025-05-03 10:31:54 +0300

    Name filter working

--- lib/edit_kennel/main.dart
+++ lib/edit_kennel/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mush_on/provider.dart';
+import 'package:mush_on/services/error_handling.dart';
 import 'package:mush_on/services/models.dart';
 import 'package:mush_on/shared/dog_filter/main.dart';
 import 'package:provider/provider.dart';
 
 class EditKennelMain extends StatelessWidget {
+  static final BasicLogger logger = BasicLogger();
   const EditKennelMain({super.key});
 
   @override
@@ -28,7 +30,16 @@ class EditKennelMain extends StatelessWidget {
             ),
             border: Border.all(color: Colors.grey),
           ),
-          child: DogFilterWidget(),
+          child: DogFilterWidget(
+            onResult: (v) {
+              if (v.isEmpty) logger.debug("Empty list");
+              String toPrint = "";
+              for (Dog d in v) {
+                toPrint = "$toPrint, ${d.name}";
+              }
+              logger.debug("RESULT of filter: $toPrint");
+            },
+          ),
         ),
         ...dogProvider.dogs.map(
           (dog) => DogCard(dog: dog),
--- lib/shared/dog_filter/provider.dart
+++ lib/shared/dog_filter/provider.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:mush_on/services/error_handling.dart';
 import 'package:mush_on/services/models/dog.dart';
+import 'package:mush_on/shared/dog_filter/enums.dart';
 
 class DogFilterProvider extends ChangeNotifier {
   BasicLogger logger = BasicLogger();
   List<Dog> _dogs = [];
   List<Dog> get dogs => _dogs;
+  List<ConditionSelectionElement> conditions = [];
+  ConditionType conditionType = ConditionType.and;
+
   DogFilterProvider();
+
   void setDogs(List<Dog> newDogs) {
     _dogs = newDogs;
     notifyListeners();
   }
+
+  /// Creates or update a certain condition in the list
+  void setCondition(
+      {required int position,
+      ConditionSelection? conditionSelection,
+      OperationSelection? operationSelection,
+      String? filterSelection}) {
+    ConditionSelectionElement toSet = conditions.firstWhere(
+        (condition) => condition.position == position,
+        orElse: () => ConditionSelectionElement(position: position));
+    if (conditionSelection != null) {
+      toSet.conditionSelection = conditionSelection;
+      toSet.operationSelection = null;
+    }
+    if (operationSelection != null) {
+      toSet.operationSelection = operationSelection;
+    }
+    if (filterSelection != null && filterSelection != "") {
+      toSet.filterSelection = filterSelection;
+    }
+
+    conditions.removeWhere((item) => item.position == position);
+    conditions.add(toSet);
+    logger.debug(
+        "Condition: ${toSet.filterSelection.toString()} - ${toSet.operationSelection.toString()} - ${toSet.conditionSelection} - ${toSet.position}");
+    notifyListeners();
+  }
+
+  /// Resets the conditions for when the page is reloaded
+  void resetConditions() => conditions = [];
+}
+
+class ConditionSelectionElement {
+  final int position;
+  ConditionSelection? conditionSelection;
+  OperationSelection? operationSelection;
+  String? filterSelection;
+
+  ConditionSelectionElement(
+      {required this.position,
+      this.conditionSelection,
+      this.operationSelection,
+      this.filterSelection});
 }
