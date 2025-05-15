4cf70376ccec007210109579eeea394518fbe93c
Bucciamarcia <info@stefanomini.com>
2025-05-15 10:05:59 +0300

    Init dog filter widget test

---
+++ test/fake_dogs.dart
@@ -0,0 +1,7 @@
+import 'package:mush_on/services/models.dart';
+
+final List<Dog> fakeDogs = [
+  Dog(name: "Fido", id: "id_Fido", positions: DogPositions(lead: true)),
+  Dog(name: "Fluffy", id: "id_Fluffy", positions: DogPositions(swing: true)),
+  Dog(name: "Wheeler", id: "id_Wheeler", positions: DogPositions(wheel: true)),
+];
