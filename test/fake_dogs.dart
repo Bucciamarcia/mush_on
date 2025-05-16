c0260db276090d2c807a018d0e7438aefff28f6d
Bucciamarcia <info@stefanomini.com>
2025-05-16 12:43:59 +0300

    Adds tests for filtering

--- test/fake_dogs.dart
+++ test/fake_dogs.dart
@@ -1,7 +1,16 @@
 import 'package:mush_on/services/models.dart';
 
 final List<Dog> fakeDogs = [
-  Dog(name: "Fido", id: "id_Fido", positions: DogPositions(lead: true)),
+  Dog(
+    name: "Fido",
+    id: "id_Fido",
+    positions: DogPositions(lead: true),
+    birth: DateTime(2000, 1, 1),
+    tags: [
+      Tag(created: DateTime(2000, 1, 1), id: "1", name: "dog"),
+    ],
+    sex: DogSex.male,
+  ),
   Dog(name: "Fluffy", id: "id_Fluffy", positions: DogPositions(swing: true)),
   Dog(name: "Wheeler", id: "id_Wheeler", positions: DogPositions(wheel: true)),
 ];
