f5c674dc0c834f5ecde4345ed71aee82d7a37fe6
Bucciamarcia <info@stefanomini.com>
2025-05-21 20:46:21 +0300

    reverts bad search error management

--- lib/create_team/model.dart
+++ lib/create_team/model.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:mush_on/services/models.dart';
-
-/// Represents a dog in the create team display widget.
-/// Contains the dog itself, and the list of errors it needs to display.
-class CreateDog {
-  final Dog dog;
-  List<SearchDogError> errors;
-  CreateDog({required this.dog, required this.errors});
-}
 
 /// All the errors that a CreateDog can have.
 enum SearchDogError {
