51107c8abeecc60369b6faadd38dd52dcfca1866
Bucciamarcia <info@stefanomini.com>
2025-05-21 20:55:16 +0300

    bla

--- lib/create_team/model.dart
+++ lib/create_team/model.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/widgets.dart';
+import 'package:mush_on/services/models.dart';
+
+class DogError {
+  final Dog dog;
+  List<DogErrorMessage> dogErrorMessages;
+
+  DogError({required this.dog, required this.dogErrorMessages});
+}
 
 /// All the errors that a CreateDog can have.
-enum SearchDogError {
+enum DogErrorMessage {
   duplicate(
       color: Color.fromARGB(255, 255, 0, 0),
       isFatal: true,
@@ -10,7 +18,7 @@ enum SearchDogError {
   final Color color;
   final String message;
   final bool isFatal;
-  const SearchDogError(
+  const DogErrorMessage(
       {required this.color, required this.message, required this.isFatal});
 }
 
