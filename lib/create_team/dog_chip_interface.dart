146f32e8999837e72996179540ecb4b85d9a6cda
Bucciamarcia <info@stefanomini.com>
2025-06-03 14:24:02 +0300

    Replaces duplicatedogs with dogerrors

--- lib/create_team/dog_chip_interface.dart
+++ lib/create_team/dog_chip_interface.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:mush_on/create_team/model.dart';
 import 'package:mush_on/services/error_handling.dart';
 import 'package:mush_on/services/models.dart';
 
 /// This is simply the logic between the main DogSelectedChip
-/// And the DogChipWarnings if the dog is duplicate
-/// (will turn into generic warnings in future)
+///
+/// It will display when a dog in a certain position has been selected
 class DogSelectedInterface extends StatelessWidget {
   final Dog dog;
-  final bool isDuplicate;
+  final List<DogError> errors;
   final Function() onDogRemoved;
 
   const DogSelectedInterface(
       {super.key,
       required this.dog,
-      required this.isDuplicate,
+      required this.errors,
       required this.onDogRemoved});
 
   @override
   Widget build(BuildContext context) {
+    DogError? dogError = DogErrorRepository.findById(errors, dog.id);
     return Column(
       children: [
         DogSelectedChip(
           dog: dog,
+          errorType: DogErrorRepository.worstErrorType(
+              dogError == null ? [] : dogError.dogErrorMessages),
           onDogRemoved: () => onDogRemoved(),
         ),
-        (isDuplicate == true) ? DogChipWarnings() : SizedBox.shrink(),
+        dogError != null ? ErrorsList(errors: dogError) : SizedBox.shrink(),
       ],
     );
   }
@@ -33,17 +37,20 @@ class DogSelectedInterface extends StatelessWidget {
 /// The chip itself, displaying the dog name and info.
 class DogSelectedChip extends StatelessWidget {
   final Dog dog;
+  final ErrorType errorType;
   final Function() onDogRemoved;
   static final BasicLogger logger = BasicLogger();
   const DogSelectedChip(
-      {super.key, required this.dog, required this.onDogRemoved});
+      {super.key,
+      required this.dog,
+      required this.onDogRemoved,
+      required this.errorType});
 
   @override
   Widget build(BuildContext context) {
     return InputChip(
       padding: EdgeInsets.all(10),
-      backgroundColor: Colors.green[200],
-      side: BorderSide(color: Colors.greenAccent, width: 3),
+      backgroundColor: errorType.color,
       key: Key("DogSelectedChip - ${dog.id}"),
       label: Text(
         dog.name,
@@ -56,19 +63,19 @@ class DogSelectedChip extends StatelessWidget {
   }
 }
 
-/// The warning for duplicate dogs. Will turn into generic warnings.
-class DogChipWarnings extends StatelessWidget {
-  const DogChipWarnings({
-    super.key,
-  });
+class ErrorsList extends StatelessWidget {
+  final DogError errors;
+  const ErrorsList({super.key, required this.errors});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "this dog is duplicate!",
-      overflow: TextOverflow.visible,
-      softWrap: true,
-      style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+    return Column(
+      children: errors.dogErrorMessages
+          .map((e) => Text(
+                e.message,
+                style: TextStyle(color: e.color),
+              ))
+          .toList(),
     );
   }
 }
