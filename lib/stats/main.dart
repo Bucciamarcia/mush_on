45cfab8ee85733a308129bb74ce15ef3788ed076
Stefano Mini <info@stefanominiconsulting.com>
2025-06-09 08:14:13 +0300

    Stats now return 30 days and ready for date range

--- lib/stats/main.dart
+++ lib/stats/main.dart
@@ -12,13 +12,24 @@ class StatsMain extends StatelessWidget {
   Widget build(BuildContext context) {
     final statsProvider = context.watch<StatsProvider>();
     final dogs = Provider.of<DogProvider>(context, listen: true).dogs;
+    DateTime startDate = _nowOnlyDay().subtract(Duration(days: 30));
+    DateTime finishDate = _nowOnlyDay();
 
-    GridRowProcessor dataManipulator =
-        GridRowProcessor(teams: statsProvider.teams, dogs: dogs);
+    GridRowProcessor dataManipulator = GridRowProcessor(
+        teams: statsProvider.teams,
+        dogs: dogs,
+        startDate: startDate,
+        finishDate: finishDate);
     GridRowProcessorResult gridData = dataManipulator.run();
 
     StatsDataSource statsDataSource = StatsDataSource(gridData: gridData);
 
     return SfDataGridClass(statsDataSource: statsDataSource, dogs: dogs);
+  }
+
+  /// Returns today, without the time.
+  DateTime _nowOnlyDay() {
+    return DateTime.utc(DateTime.now().toUtc().year,
+        DateTime.now().toUtc().month, DateTime.now().toUtc().day);
   }
 }
