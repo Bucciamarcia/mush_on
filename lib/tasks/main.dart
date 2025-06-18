9c6c02e83aa90f188f6b032cdf06f9f31d87e01b
Stefano Mini <info@stefanominiconsulting.com>
2025-06-18 12:11:21 +0300

    Can add tasks to db

--- lib/tasks/main.dart
+++ lib/tasks/main.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:mush_on/provider.dart';
+import 'package:mush_on/services/models/tasks.dart';
+import 'package:provider/provider.dart';
 
 class TasksMainWidget extends StatelessWidget {
   const TasksMainWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    MainProvider provider = context.watch<MainProvider>();
+    return ElevatedButton(
+      onPressed: () async => await _showAddTaskDialog(
+          context: context,
+          onTaskAdded: (newTask) async {
+            await provider.addTask(newTask);
+          }),
+      child: Text("Add a task"),
+    );
+  }
+
+  Future<AlertDialog> _showAddTaskDialog(
+      {required BuildContext context,
+      required Function(Task) onTaskAdded}) async {
+    return await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog.adaptive(
+            title: Text("Add task"),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text("Cancel"),
+              ),
+              TextButton(
+                onPressed: () {
+                  onTaskAdded(Task());
+                  Navigator.of(context).pop();
+                },
+                child: Text("Save new task"),
+              ),
+            ],
+          );
+        });
   }
 }
