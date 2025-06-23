a82ff541d53948edd8254205e8198a363bb6ff0b
Stefano Mini <info@stefanominiconsulting.com>
2025-06-23 12:17:18 +0300

    Tasks are okay for now

--- lib/tasks/tab_bar_widgets/main.dart
+++ lib/tasks/tab_bar_widgets/main.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:mush_on/services/models/dog.dart';
 import 'package:mush_on/services/models/tasks.dart';
+import 'package:mush_on/shared/text_title.dart';
 import 'package:mush_on/tasks/tab_bar_widgets/now_tab_view.dart';
 
 import 'calendar/main.dart';
@@ -24,12 +26,26 @@ class TabBarViewWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return TabBarView(
       children: [
-        SingleChildScrollView(
-          child: NowTabView(
-            tasks: tasks,
-            dogs: dogs,
-            onTaskEdited: (t) => onTaskEdited(t),
-          ),
+        NowTabView(
+          onFetchOlderTasks: (date) => onFetchOlderTasks(date),
+          tasksInMemory: tasks,
+          dogs: dogs,
+          onTaskEdited: (t) => onTaskEdited(t),
+        ),
+        Column(
+          children: [
+            TextTitle(
+                "Non-expiring tasks (${tasks.tasks.dontExpire.notDone.length})"),
+            ...tasks.tasks.notDone.dontExpire.urgentFirst().map(
+                  (t) => TaskElement(
+                    dogs: dogs,
+                    key: ValueKey(t.id),
+                    dog: dogs.firstWhereOrNull((d) => d.id == t.dogId),
+                    task: t,
+                    onTaskEdited: (t) => onTaskEdited(t),
+                  ),
+                )
+          ],
         ),
         CalendarTabWidget(
           onFetchOlderTasks: (d) => onFetchOlderTasks(d),
