f579c1365957c69da34956dbc888b6166471063e
Stefano Mini <info@stefanominiconsulting.com>
2025-06-25 16:27:17 +0300

    Init home page provider

---
+++ lib/home_page/provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mush_on/provider.dart';
+
+class HomePageProvider extends ChangeNotifier {
+  final MainProvider mainProvider;
+
+  HomePageProvider(this.mainProvider);
+}
