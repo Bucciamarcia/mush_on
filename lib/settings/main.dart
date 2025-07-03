a7d0d5ddcca581146d33933707125cb8ab7ef902
Stefano Mini <info@stefanominiconsulting.com>
2025-07-03 11:45:06 +0300

    Switch settings to riverpod

--- lib/settings/main.dart
+++ lib/settings/main.dart
@@ -1,76 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mush_on/riverpod.dart';
 import 'package:mush_on/services/error_handling.dart';
-import 'package:mush_on/services/models/settings/settings.dart';
 import 'package:mush_on/settings/custom_fields.dart';
-import 'package:mush_on/settings/provider.dart';
-import 'package:mush_on/settings/save_cancel_buttons.dart';
+import 'package:mush_on/settings/riverpod.dart';
 import 'package:mush_on/shared/distance_warning_widget/main.dart';
 import 'package:mush_on/shared/text_title.dart';
-import 'package:provider/provider.dart';
 
-class SettingsMain extends StatefulWidget {
+class SettingsMain extends ConsumerStatefulWidget {
   static final BasicLogger logger = BasicLogger();
   const SettingsMain({super.key});
 
   @override
-  State<SettingsMain> createState() => _SettingsMainState();
+  ConsumerState<SettingsMain> createState() => _SettingsMainState();
 }
 
-class _SettingsMainState extends State<SettingsMain> {
-  Future<bool> _showExitConfirmationDialog() async {
-    return await showDialog<bool>(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: const Text('Discard Changes?'),
-              content: const Text(
-                  'You have unsaved changes. Are you sure you want to exit?'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(false); // Stay on page
-                  },
-                  child: const Text('Stay'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(true); // Discard and exit
-                  },
-                  child: const Text('Discard'),
-                ),
-              ],
-            );
-          },
-        ) ??
-        false; // Return false if dialog is dismissed
-  }
-
+class _SettingsMainState extends ConsumerState<SettingsMain> {
   @override
   Widget build(BuildContext context) {
-    SettingsProvider settingsProvider = context.watch<SettingsProvider>();
-    SettingsModel settings = settingsProvider.settings;
-    return PopScope(
-      canPop: !settingsProvider.didSomethingChange,
-      onPopInvokedWithResult: (didpop, result) async {
-        if (didpop) {
-          return;
-        }
-        final bool shouldPop = await _showExitConfirmationDialog();
-        if (shouldPop) {
-          // If the user confirmed to discard, then manually pop the route.
-          // This will now succeed as we've confirmed the user's intent.
-          if (context.mounted) Navigator.of(context).pop();
-        }
-      },
-      child: settingsProvider.isInitLoading
-          ? Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: const EdgeInsets.all(40),
-                child: CircularProgressIndicator.adaptive(),
-              ),
-            )
-          : SingleChildScrollView(
+    var settingsAsync = ref.watch(settingsProvider);
+    return settingsAsync.when(
+      data: (settings) {
+        var settingsRepoAsync = ref.watch(settingsRepositoryProvider);
+        return settingsRepoAsync.when(
+          data: (settingsRepo) {
+            return SingleChildScrollView(
               child: Align(
                 alignment: Alignment.topCenter,
                 child: Column(
@@ -80,60 +34,17 @@ class _SettingsMainState extends State<SettingsMain> {
                     CustomFieldsOptions(
                       customFieldTemplates: settings.customFieldTemplates,
                       onCustomFieldAdded: (newCustomField) =>
-                          settingsProvider.addCustomField(newCustomField),
+                          settingsRepo.addCustomField(newCustomField, settings),
                       onCustomFieldDeleted: (id) =>
-                          settingsProvider.deleteCustomField(id),
-                    ),
-                    SaveCancelButtons(
-                      onSavePressed: () async {
-                        try {
-                          await settingsProvider.saveSettingsToDb();
-                          if (context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(
-                                  "Settings saved successfully",
-                                  style: TextStyle(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onPrimary),
-                                ),
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.primary,
-                              ),
-                            );
-                          }
-                          if (context.mounted) Navigator.of(context).pop();
-                        } catch (e, s) {
-                          SettingsMain.logger.error("Couldn't save settings",
-                              error: e, stackTrace: s);
-                          if (context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              errorSnackBar(context, "Couldn't save settings"),
-                            );
-                          }
-                        }
-                      },
-                      onCancelPressed: () async {
-                        if (settingsProvider.didSomethingChange) {
-                          final bool shouldPop =
-                              await _showExitConfirmationDialog();
-                          if (shouldPop && context.mounted) {
-                            Navigator.of(context).pop();
-                          }
-                        } else {
-                          Navigator.of(context).pop();
-                        }
-                      },
-                      didSomethingChange: settingsProvider.didSomethingChange,
+                          settingsRepo.deleteCustomField(id, settings),
                     ),
                     TextTitle("Global distance warnings"),
                     DistanceWarningWidget(
-                        warnings:
-                            settingsProvider.settings.globalDistanceWarnings,
+                        warnings: settings.globalDistanceWarnings,
                         onWarningAdded: (warning) async {
                           try {
-                            await settingsProvider.addWarning(warning);
+                            await settingsRepo.addDistanceWarning(
+                                warning, settings);
                           } catch (e, s) {
                             SettingsMain.logger.error(
                                 "Couldn't add distance warning",
@@ -148,7 +59,8 @@ class _SettingsMainState extends State<SettingsMain> {
                         },
                         onWarningEdited: (warning) async {
                           try {
-                            await settingsProvider.editWarning(warning);
+                            await settingsRepo.editDistanceWarning(
+                                warning, settings);
                           } catch (e, s) {
                             SettingsMain.logger.error(
                                 "Couldn't edit distance warning",
@@ -163,7 +75,8 @@ class _SettingsMainState extends State<SettingsMain> {
                         },
                         onWarningRemoved: (id) async {
                           try {
-                            await settingsProvider.removeWarning(id);
+                            await settingsRepo.removeDistanceWarning(
+                                id, settings);
                           } catch (e, s) {
                             SettingsMain.logger.error(
                                 "Couldn't remove distance warning",
@@ -179,7 +92,20 @@ class _SettingsMainState extends State<SettingsMain> {
                   ],
                 ),
               ),
-            ),
+            );
+          },
+          error: (e, s) {
+            BasicLogger().error("Couldn't load settings repo");
+            return Text("Error: couldn't load settings");
+          },
+          loading: () => Center(child: CircularProgressIndicator.adaptive()),
+        );
+      },
+      error: (e, s) {
+        BasicLogger().error("Couldn't load settings", error: e, stackTrace: s);
+        return Text("Couldn't load settings");
+      },
+      loading: () => Center(child: CircularProgressIndicator.adaptive()),
     );
   }
 }
---
+++ lib/settings/riverpod.dart
@@ -0,0 +1,107 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mush_on/riverpod.dart';
+import 'package:mush_on/services/error_handling.dart';
+import 'package:mush_on/services/models/settings/custom_field.dart';
+import 'package:mush_on/services/models/settings/distance_warning.dart';
+import 'package:mush_on/services/models/settings/settings.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'riverpod.g.dart';
+
+@riverpod
+Future<SettingsRepository> settingsRepository(Ref ref) async {
+  String account = await ref.watch(accountProvider.future);
+  return SettingsRepository(account: account);
+}
+
+class SettingsRepository {
+  final db = FirebaseFirestore.instance;
+  final String account;
+  static BasicLogger logger = BasicLogger();
+
+  SettingsRepository({required this.account});
+
+  Future<void> addCustomField(
+      CustomFieldTemplate cf, SettingsModel currentSettings) async {
+    var newSettings = currentSettings.copyWith(
+        customFieldTemplates: [...currentSettings.customFieldTemplates, cf]);
+    String path = "accounts/$account/data/settings";
+    var doc = db.doc(path);
+    try {
+      await doc.set(newSettings.toJson());
+    } catch (e, s) {
+      logger.error("Couldn't add custom field to db", error: e, stackTrace: s);
+      rethrow;
+    }
+  }
+
+  Future<void> deleteCustomField(
+      String id, SettingsModel currentSettings) async {
+    List<CustomFieldTemplate> templates =
+        List.from(currentSettings.customFieldTemplates);
+    templates.removeWhere((t) => t.id == id);
+    SettingsModel newSettings =
+        currentSettings.copyWith(customFieldTemplates: templates);
+    String path = "accounts/$account/data/settings";
+    var doc = db.doc(path);
+    try {
+      await doc.set(newSettings.toJson());
+    } catch (e, s) {
+      logger.error("Couldn't delete custom field from db",
+          error: e, stackTrace: s);
+      rethrow;
+    }
+  }
+
+  Future<void> addDistanceWarning(
+      DistanceWarning warning, SettingsModel currentSettings) async {
+    List<DistanceWarning> warnings =
+        List.from(currentSettings.globalDistanceWarnings);
+    warnings.add(warning);
+    SettingsModel newSettings =
+        currentSettings.copyWith(globalDistanceWarnings: warnings);
+    String path = "accounts/$account/data/settings";
+    var doc = db.doc(path);
+    try {
+      await doc.set(newSettings.toJson());
+    } catch (e, s) {
+      logger.error("Couldn't add distance warning", error: e, stackTrace: s);
+      rethrow;
+    }
+  }
+
+  Future<void> editDistanceWarning(
+      DistanceWarning warning, SettingsModel currentSettings) async {
+    List<DistanceWarning> warnings =
+        List.from(currentSettings.globalDistanceWarnings);
+    warnings.removeWhere((w) => w.id == warning.id);
+    warnings.add(warning);
+    SettingsModel newSettings =
+        currentSettings.copyWith(globalDistanceWarnings: warnings);
+    String path = "accounts/$account/data/settings";
+    var doc = db.doc(path);
+    try {
+      await doc.set(newSettings.toJson());
+    } catch (e, s) {
+      logger.error("Couldn't add distance warning", error: e, stackTrace: s);
+      rethrow;
+    }
+  }
+
+  Future<void> removeDistanceWarning(
+      String id, SettingsModel currentSettings) async {
+    List<DistanceWarning> warnings =
+        List.from(currentSettings.globalDistanceWarnings);
+    warnings.removeWhere((w) => w.id == id);
+    SettingsModel newSettings =
+        currentSettings.copyWith(globalDistanceWarnings: warnings);
+    String path = "accounts/$account/data/settings";
+    var doc = db.doc(path);
+    try {
+      await doc.set(newSettings.toJson());
+    } catch (e, s) {
+      logger.error("Couldn't add distance warning", error: e, stackTrace: s);
+      rethrow;
+    }
+  }
+}
