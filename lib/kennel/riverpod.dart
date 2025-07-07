fcb7ca1ed70f09286f9c66b8c26fed45bff3a893
Stefano Mini <info@stefanominiconsulting.com>
2025-07-07 13:25:51 +0300

    removes provider from kennel page

---
+++ lib/kennel/riverpod.dart
@@ -0,0 +1,21 @@
+import 'package:mush_on/riverpod.dart';
+import 'package:mush_on/services/models/dog.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'riverpod.g.dart';
+
+@riverpod
+class DogsDisplayList extends _$DogsDisplayList {
+  @override
+  List<Dog> build() {
+    var initialDogs = ref.read(dogsProvider).valueOrNull ?? [];
+    var dogsCopy = List<Dog>.from(initialDogs);
+    dogsCopy.sort((a, b) => a.name.compareTo(b.name));
+    return dogsCopy;
+  }
+
+  void setDogs(List<Dog> dogs) {
+    var dogsCopy = List<Dog>.from(dogs);
+    dogsCopy.sort((a, b) => a.name.compareTo(b.name));
+    state = dogsCopy;
+  }
+}
--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mush_on/page_template.dart';
-import 'package:mush_on/provider.dart';
-import 'package:mush_on/settings/provider.dart';
-import 'package:provider/provider.dart';
-
 import 'main.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -11,16 +7,9 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var mainProvider = context.watch<MainProvider>();
-    return ChangeNotifierProvider(
-      create: (context) {
-        final settingsProvider = SettingsProvider(mainProvider);
-        return settingsProvider;
-      },
-      child: TemplateScreen(
-        title: "Settings",
-        child: SettingsMain(),
-      ),
+    return TemplateScreen(
+      title: "Settings",
+      child: SettingsMain(),
     );
   }
 }
