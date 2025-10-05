e30aac63b402692d160eb25ad0541e68a69c9c9f
Stefano Mini <info@stefanominiconsulting.com>
2025-10-05 17:22:56 +0300

    Init participants box

--- lib/customer_facing/booking/success/booking_success.dart
+++ lib/customer_facing/booking/success/booking_success.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:mush_on/customer_management/models.dart';
 import 'package:mush_on/services/error_handling.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -34,7 +35,8 @@ class BookingSuccessPage extends ConsumerWidget {
                 return const Text(
                     "Error: couldn't load the booking. Contact the kennel.");
               },
-              loading: () => const CircularProgressIndicator.adaptive()),
+              loading: () =>
+                  const Center(child: CircularProgressIndicator.adaptive())),
         ),
       ),
     );
@@ -54,8 +56,66 @@ class BookingConfirmationDataPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
     final urlAndAmount = ref.watch(receiptUrlProvider(booking.id)).value;
-    return const Placeholder();
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 600),
+      child: SingleChildScrollView(
+        child: Column(
+          spacing: 25,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    colorScheme.primaryContainer,
+                    colorScheme.primaryContainer.withAlpha(20),
+                  ],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    margin: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                          color: colorScheme.primary.withAlpha(150),
+                          blurRadius: 15,
+                          spreadRadius: 3)
+                    ], shape: BoxShape.circle, color: Colors.white),
+                    child: Icon(
+                      Icons.check_circle,
+                      color: colorScheme.primary,
+                      weight: 60,
+                      size: 80,
+                    ),
+                  ),
+                  const Text(
+                    "Booking confirmed!",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w800),
+                    textAlign: TextAlign.center,
+                  ),
+                  const Text(
+                    "See the details below",
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+            TourDetailsBox(booking: booking, customers: customers, cg: cg),
+            ParticipantsBox(booking: booking, customers: customers, cg: cg),
+          ],
+        ),
+      ),
+    );
   }
 
   Future<void> launchReceiptUrl(String url) async {
@@ -67,5 +127,180 @@ class BookingConfirmationDataPage extends ConsumerWidget {
     )) {
       throw Exception("Could not launch $url");
     }
+  }
+}
+
+class TourDetailsBox extends StatelessWidget {
+  final Booking booking;
+  final List<Customer> customers;
+  final CustomerGroup cg;
+  const TourDetailsBox({
+    super.key,
+    required this.booking,
+    required this.customers,
+    required this.cg,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: BoxBorder.all(color: colorScheme.primary.withAlpha(50)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      child: Column(
+        spacing: 20,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            spacing: 20,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      colorScheme.primaryContainer,
+                      colorScheme.primaryContainer.withAlpha(150)
+                    ]),
+                    borderRadius: const BorderRadius.all(Radius.circular(10))),
+                child: Icon(
+                  Icons.calendar_today,
+                  color: colorScheme.primary,
+                  size: 30,
+                ),
+              ),
+              const Text(
+                "Tour Details",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+          BoxElement(
+            iconData: Icons.calendar_month,
+            title: "Date",
+            content: DateFormat("dd-MM-yyyy").format(cg.datetime),
+          ),
+          BoxElement(
+            iconData: Icons.punch_clock,
+            title: "Time",
+            content: DateFormat("hh:mm").format(cg.datetime),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ParticipantsBox extends StatelessWidget {
+  final Booking booking;
+  final List<Customer> customers;
+  final CustomerGroup cg;
+  const ParticipantsBox({
+    super.key,
+    required this.booking,
+    required this.customers,
+    required this.cg,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: BoxBorder.all(color: colorScheme.primary.withAlpha(50)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      child: Column(
+        spacing: 20,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            spacing: 20,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      colorScheme.primaryContainer,
+                      colorScheme.primaryContainer.withAlpha(150)
+                    ]),
+                    borderRadius: const BorderRadius.all(Radius.circular(10))),
+                child: Icon(
+                  Icons.calendar_today,
+                  color: colorScheme.primary,
+                  size: 30,
+                ),
+              ),
+              const Text(
+                "Tour Details",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+          BoxElement(
+            iconData: Icons.calendar_month,
+            title: "Date",
+            content: DateFormat("dd-MM-yyyy").format(cg.datetime),
+          ),
+          BoxElement(
+            iconData: Icons.punch_clock,
+            title: "Time",
+            content: DateFormat("hh:mm").format(cg.datetime),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BoxElement extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  final String content;
+  const BoxElement({
+    super.key,
+    required this.iconData,
+    required this.title,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Row(
+      spacing: 20,
+      children: [
+        Icon(
+          iconData,
+          color: colorScheme.primary,
+          size: 26,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
+            ),
+            Text(
+              content,
+              style: const TextStyle(fontSize: 16),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
