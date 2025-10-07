c9ba197cae017d5094531c8764bf145f7603693e
Stefano Mini <info@stefanominiconsulting.com>
2025-10-07 17:03:26 +0300

    TODO: set up postmark

--- functions/lib/add_booking/add_checkout_session.py
+++ functions/lib/add_booking/add_checkout_session.py
@@ -1,6 +1,8 @@
 from firebase_admin import firestore
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
+
+from lib.stripe.get_payment_receipt_url import get_payment_receipt_url
 
 
 class CheckoutSession(BaseModel):
@@ -10,6 +12,28 @@ class CheckoutSession(BaseModel):
     bookingId: str
     createdAt: datetime
     webhookProcessed: bool
+
+
+class Booking(BaseModel):
+    city: str
+    country: str
+    customerGroupId: str
+    email: str
+    id: str
+    name: str
+    paymentStatus: str
+    phone: str
+    streetAddress: str
+    zipCode: str
+
+
+class CustomerGroup(BaseModel):
+    datetime: datetime
+    id: str
+    maxCapacity: int
+    name: str
+    teamGroupId: str | None = Field(default=None)
+    tourTypeId: str | None = Field(default=None)
 
 
 def add_checkout_session_to_db(
@@ -27,7 +51,9 @@ def add_checkout_session_to_db(
     )
 
 
-def payment_processed(checkout_session_id: str) -> None:
+def payment_processed(
+    checkout_session_id: str, account: str, stripe_api_key: str
+) -> None:
     """Sets webook processed in checkout sessions and changes payment
     status in booking"""
     path = f"checkoutSessions/{checkout_session_id}"
@@ -45,10 +71,28 @@ def payment_processed(checkout_session_id: str) -> None:
     batch.update(doc_ref, {"webhookProcessed": True})
     booking_path = f"accounts/{checkout_session.account}/data/bookingManager/bookings/{checkout_session.bookingId}"
     booking_ref = firestore.client().document(booking_path)
-    booking = booking_ref.get()
-    data = booking.to_dict()
+    booking_obj = booking_ref.get()
+    data = booking_obj.to_dict()
+    booking = Booking.model_validate(data)
     if data is None:
         raise Exception("Couldn't find booking")
-    email = data["email"]
     batch.update(booking_ref, {"paymentStatus": "paid"})
     batch.commit()
+    email = booking.email
+    cg_obj = (
+        firestore.client()
+        .document(
+            f"accounts/{checkout_session.account}/data/bookingManager/customerGroups/{booking.customerGroupId}"
+        )
+        .get()
+    )
+    cg_data = cg_obj.to_dict()
+    cg = CustomerGroup.model_validate(cg_data)
+    payment_receipt_url = get_payment_receipt_url(
+        stripe_id=account,
+        checkout_session_id=checkout_session_id,
+        stripe_api_key=stripe_api_key,
+    )
+    url = payment_receipt_url["url"]
+    total = payment_receipt_url["total"]
+    # TODO: Add postmark integration
