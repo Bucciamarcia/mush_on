81890e6638382aa8ec5e5ee328e5b7ba7d3059e0
Stefano Mini <info@stefanominiconsulting.com>
2025-10-17 10:34:19 +0300

    init add users

---
+++ lib/settings/add_users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddUsers extends StatelessWidget {
+  const AddUsers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
