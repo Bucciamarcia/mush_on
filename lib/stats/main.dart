3c7dab3639d1f5259a221be7ce12e0ce57545fb4
Stefano Mini <info@stefanominiconsulting.com>
2025-11-14 11:31:34 +0200

    Fix stats page

--- lib/stats/main.dart
+++ lib/stats/main.dart
@@ -28,7 +28,7 @@ class StatsMain extends ConsumerWidget {
         var dates = ref.watch(statsDatesProvider);
         var teamGroupsAsync = ref.watch(teamGroupsProvider(
             earliestDate:
-                DateTimeUtils.today().subtract(const Duration(days: 360)),
+                DateTimeUtils.today().subtract(const Duration(days: 30)),
             finalDate: DateTimeUtils.endOfToday()));
         return teamGroupsAsync.when(
           data: (teams) {
