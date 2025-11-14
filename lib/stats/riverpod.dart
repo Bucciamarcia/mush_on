5c5092856abe4cdedc7d43100dd76ec1138c7de8
Stefano Mini <info@stefanominiconsulting.com>
2025-11-14 13:54:32 +0200

    only 14 days for stats

--- lib/stats/riverpod.dart
+++ lib/stats/riverpod.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mush_on/riverpod.dart';
 import 'package:mush_on/services/extensions.dart';
-import 'package:mush_on/services/models/dog.dart';
+import 'package:mush_on/services/models.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'riverpod.freezed.dart';
 part 'riverpod.g.dart';
@@ -46,4 +49,81 @@ class FilteredDogs extends _$FilteredDogs {
   void changeFilteredDogs(List<Dog> newDogs) {
     state = newDogs;
   }
+}
+
+/// Pre-fetched data structure to avoid creating hundreds of reactive listeners
+class TeamGroupData {
+  final Map<String, List<Team>> teamsByGroupId;
+  final Map<String, Map<String, List<DogPair>>> dogPairsByGroupAndTeamId;
+
+  TeamGroupData({
+    required this.teamsByGroupId,
+    required this.dogPairsByGroupAndTeamId,
+  });
+}
+
+@riverpod
+Future<TeamGroupData> prefetchedTeamData(
+    Ref ref, List<TeamGroup> teamGroups) async {
+  final account = await ref.watch(accountProvider.future);
+  final db = FirebaseFirestore.instance;
+
+  // Get all teamGroup IDs we care about
+  final teamGroupIds = teamGroups.map((tg) => tg.id).toSet();
+
+  final teamsByGroupId = <String, List<Team>>{};
+  final dogPairsByGroupAndTeamId = <String, Map<String, List<DogPair>>>{};
+
+  // Batch the queries to work around Firestore limits and parallelize
+  final teamGroupIdsList = teamGroupIds.toList();
+  final allTeamsData = <String, List<Team>>{};
+
+  for (int i = 0; i < teamGroupIdsList.length; i += 10) {
+    final batch = teamGroupIdsList.skip(i).take(10).toList();
+
+    // Fetch teams for each teamGroup in parallel
+    final futures = batch.map((teamGroupId) =>
+      db.collection('accounts/$account/data/teams/history/$teamGroupId/teams')
+        .orderBy('rank')
+        .get()
+    ).toList();
+
+    final snapshots = await Future.wait(futures);
+    for (int j = 0; j < batch.length; j++) {
+      final teams = snapshots[j].docs
+          .map((doc) => Team.fromJson(doc.data()))
+          .toList();
+      allTeamsData[batch[j]] = teams;
+    }
+  }
+
+  teamsByGroupId.addAll(allTeamsData);
+
+  // Now fetch all dogPairs for all teams in parallel
+  final allDogPairsData = <String, Map<String, List<DogPair>>>{};
+
+  for (final teamGroupId in teamGroupIds) {
+    final teams = teamsByGroupId[teamGroupId] ?? [];
+    if (teams.isEmpty) continue;
+
+    final dogPairFutures = teams.map((team) =>
+      db.collection('accounts/$account/data/teams/history/$teamGroupId/teams/${team.id}/dogPairs')
+        .orderBy('rank')
+        .get()
+        .then((snapshot) => MapEntry(
+          team.id,
+          snapshot.docs.map((doc) => DogPair.fromJson(doc.data())).toList()
+        ))
+    ).toList();
+
+    final dogPairEntries = await Future.wait(dogPairFutures);
+    allDogPairsData[teamGroupId] = Map.fromEntries(dogPairEntries);
+  }
+
+  dogPairsByGroupAndTeamId.addAll(allDogPairsData);
+
+  return TeamGroupData(
+    teamsByGroupId: teamsByGroupId,
+    dogPairsByGroupAndTeamId: dogPairsByGroupAndTeamId,
+  );
 }
--- lib/stats/riverpod.g.dart
+++ lib/stats/riverpod.g.dart
@@ -6,6 +6,162 @@ part of 'riverpod.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+String _$prefetchedTeamDataHash() =>
+    r'42f00aeb8bf9b17edfade05664a46174d1356bc7';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [prefetchedTeamData].
+@ProviderFor(prefetchedTeamData)
+const prefetchedTeamDataProvider = PrefetchedTeamDataFamily();
+
+/// See also [prefetchedTeamData].
+class PrefetchedTeamDataFamily extends Family<AsyncValue<TeamGroupData>> {
+  /// See also [prefetchedTeamData].
+  const PrefetchedTeamDataFamily();
+
+  /// See also [prefetchedTeamData].
+  PrefetchedTeamDataProvider call(
+    List<TeamGroup> teamGroups,
+  ) {
+    return PrefetchedTeamDataProvider(
+      teamGroups,
+    );
+  }
+
+  @override
+  PrefetchedTeamDataProvider getProviderOverride(
+    covariant PrefetchedTeamDataProvider provider,
+  ) {
+    return call(
+      provider.teamGroups,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'prefetchedTeamDataProvider';
+}
+
+/// See also [prefetchedTeamData].
+class PrefetchedTeamDataProvider
+    extends AutoDisposeFutureProvider<TeamGroupData> {
+  /// See also [prefetchedTeamData].
+  PrefetchedTeamDataProvider(
+    List<TeamGroup> teamGroups,
+  ) : this._internal(
+          (ref) => prefetchedTeamData(
+            ref as PrefetchedTeamDataRef,
+            teamGroups,
+          ),
+          from: prefetchedTeamDataProvider,
+          name: r'prefetchedTeamDataProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$prefetchedTeamDataHash,
+          dependencies: PrefetchedTeamDataFamily._dependencies,
+          allTransitiveDependencies:
+              PrefetchedTeamDataFamily._allTransitiveDependencies,
+          teamGroups: teamGroups,
+        );
+
+  PrefetchedTeamDataProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.teamGroups,
+  }) : super.internal();
+
+  final List<TeamGroup> teamGroups;
+
+  @override
+  Override overrideWith(
+    FutureOr<TeamGroupData> Function(PrefetchedTeamDataRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: PrefetchedTeamDataProvider._internal(
+        (ref) => create(ref as PrefetchedTeamDataRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        teamGroups: teamGroups,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeFutureProviderElement<TeamGroupData> createElement() {
+    return _PrefetchedTeamDataProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is PrefetchedTeamDataProvider &&
+        other.teamGroups == teamGroups;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, teamGroups.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin PrefetchedTeamDataRef on AutoDisposeFutureProviderRef<TeamGroupData> {
+  /// The parameter `teamGroups` of this provider.
+  List<TeamGroup> get teamGroups;
+}
+
+class _PrefetchedTeamDataProviderElement
+    extends AutoDisposeFutureProviderElement<TeamGroupData>
+    with PrefetchedTeamDataRef {
+  _PrefetchedTeamDataProviderElement(super.provider);
+
+  @override
+  List<TeamGroup> get teamGroups =>
+      (origin as PrefetchedTeamDataProvider).teamGroups;
+}
+
 String _$statsDatesHash() => r'bef6ea319d1b6e629b2e5de2768659408beb1fb0';
 
 /// See also [StatsDates].
