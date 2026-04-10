54d84ac3dc6bab79d86f34bc5bd9d2d7ac25c53f
Stefano Mini <info@stefanominiconsulting.com>
2026-04-10 09:53:14 +0300

    Connects gemini

--- lib/kennel/import_dogs/main.dart
+++ lib/kennel/import_dogs/main.dart
@@ -1,10 +1,54 @@
+import 'package:firebase_ai/firebase_ai.dart';
 import 'package:flutter/material.dart';
 
-class ImportDogsMain extends StatelessWidget {
+class ImportDogsMain extends StatefulWidget {
   const ImportDogsMain({super.key});
 
   @override
+  State<ImportDogsMain> createState() => _ImportDogsMainState();
+}
+
+class _ImportDogsMainState extends State<ImportDogsMain> {
+  late String output;
+  late bool isLoading;
+  @override
+  void initState() {
+    output = "Waiting for output";
+    isLoading = false;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text(output),
+        ElevatedButton(
+          onPressed: isLoading ? null : () async => await _callGemini(),
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : const Text("test"),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _callGemini() async {
+    setState(() {
+      isLoading = true;
+    });
+    final model = FirebaseAI.googleAI().generativeModel(
+      model: "gemini-3-flash-preview",
+    );
+    final prompt = [
+      Content.text(
+        "This is a test. Write that this is a success in a funny way",
+      ),
+    ];
+    final response = await model.generateContent(prompt);
+    setState(() {
+      output = response.text ?? "ERROR: no text";
+      isLoading = false;
+    });
   }
 }
