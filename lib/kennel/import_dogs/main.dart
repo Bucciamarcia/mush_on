2392ca2a8bbebd1f3a4da5423b873b52a9a3c1c1
Stefano Mini <info@stefanominiconsulting.com>
2026-04-10 12:05:40 +0300

    Extracts dog results

--- lib/kennel/import_dogs/main.dart
+++ lib/kennel/import_dogs/main.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_ai/firebase_ai.dart';
 import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:mush_on/services/error_handling.dart';
+part 'main.freezed.dart';
+part 'main.g.dart';
 
 class ImportDogsMain extends StatefulWidget {
   const ImportDogsMain({super.key});
@@ -25,82 +30,103 @@ class _ImportDogsMainState extends State<ImportDogsMain> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: TextField(controller: pathController, enabled: false),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() => isLoading = true);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 500),
+                child: TextField(controller: pathController, enabled: false),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  setState(() => isLoading = true);
 
-                try {
-                  final result = await FilePicker.platform.pickFiles(
-                    allowMultiple: false,
-                    type: FileType.custom,
-                    allowedExtensions: ["pdf", "csv"],
-                    withData: true,
-                  );
+                  try {
+                    final result = await FilePicker.platform.pickFiles(
+                      allowMultiple: false,
+                      type: FileType.custom,
+                      allowedExtensions: ["pdf", "csv"],
+                      withData: true,
+                    );
 
-                  if (!mounted) return;
+                    if (!mounted) return;
 
-                  if (result == null) {
+                    if (result == null) {
+                      setState(() {
+                        pathController.text = "";
+                        isLoading = false;
+                      });
+                      return;
+                    }
+
+                    final file = result.files.first;
+
+                    if (file.bytes == null) {
+                      throw Exception("File data could not be read.");
+                    }
+
                     setState(() {
-                      pathController.text = "";
+                      pathController.text = file.name;
+                      platformFile = file;
                       isLoading = false;
                     });
-                    return;
-                  }
-
-                  final file = result.files.first;
-
-                  if (file.bytes == null) {
-                    throw Exception("File data could not be read.");
-                  }
-
-                  setState(() {
-                    pathController.text = file.name;
-                    platformFile = file;
-                    isLoading = false;
-                  });
-                } catch (e, s) {
-                  BasicLogger().error(
-                    "File pick error",
-                    error: e,
-                    stackTrace: s,
-                  );
-                  if (context.mounted) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      errorSnackBar(context, "Error: ${e.toString()}"),
+                  } catch (e, s) {
+                    BasicLogger().error(
+                      "File pick error",
+                      error: e,
+                      stackTrace: s,
                     );
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        errorSnackBar(context, "Error: ${e.toString()}"),
+                      );
+                    }
+                    setState(() => isLoading = false);
                   }
-                  setState(() => isLoading = false);
-                }
-              },
-              child: const Text("Upload document"),
-            ),
-          ],
-        ),
-        ElevatedButton(
-          onPressed: isLoading ? null : () async => await _callGemini(),
-          child: isLoading
-              ? const CircularProgressIndicator()
-              : const Text("test"),
-        ),
-        Text(output),
-      ],
+                },
+                child: const Text("Upload document"),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: isLoading ? null : () async => await _callGemini(),
+            child: isLoading
+                ? const CircularProgressIndicator()
+                : const Text("test"),
+          ),
+          Text(output),
+        ],
+      ),
     );
   }
 
   Future<void> _callGemini() async {
+    final jsonSchema = Schema.object(
+      properties: {
+        "dogs": Schema.array(
+          items: Schema.string(
+            title:
+                "The name of the dog, always first letter capitalized, eg `Fido`",
+          ),
+        ),
+        "isSuccessful": Schema.boolean(
+          title: "Whether the operation is successful",
+          description:
+              "True if a list of dogs can be extracted. False if it can't be extracted or the document doesn't contain a list of dogs",
+        ),
+      },
+    );
     setState(() {
       isLoading = true;
     });
     final model = FirebaseAI.googleAI().generativeModel(
       model: "gemini-3-flash-preview",
+      generationConfig: GenerationConfig(
+        responseMimeType: "application/json",
+        responseSchema: jsonSchema,
+      ),
     );
     if (platformFile == null) {
       ScaffoldMessenger.of(
@@ -134,13 +160,43 @@ class _ImportDogsMainState extends State<ImportDogsMain> {
         ? "application/pdf"
         : "text/csv";
     final docPart = InlineDataPart(mimeType, bytes);
-    const prompt = TextPart("Briefly summarize the content of the file");
-    final response = await model.generateContent([
-      Content.multi([prompt, docPart]),
-    ]);
-    setState(() {
-      output = response.text ?? "ERROR: no text";
-      isLoading = false;
-    });
+    const prompt = TextPart("Generate a list of dogs in this document");
+    try {
+      final response = await model.generateContent([
+        Content.multi([prompt, docPart]),
+      ]);
+      final text = response.text;
+      if (text == null) {
+        throw Exception("No text returned from Gemini");
+      }
+      final imported = ImportDogResult.decode(text);
+      BasicLogger().debug("Dogs importe: ${imported.length}");
+      setState(() {
+        output = imported.toString();
+        isLoading = false;
+      });
+    } catch (e, s) {
+      BasicLogger().error("Couldn't generate content", error: e, stackTrace: s);
+      setState(() {
+        output = "Error: $e";
+        isLoading = false;
+      });
+    }
+  }
+}
+
+@freezed
+sealed class ImportDogResult with _$ImportDogResult {
+  const factory ImportDogResult({
+    required List<String> dogs,
+    required bool isSuccessful,
+  }) = _ImportDogResult;
+  factory ImportDogResult.fromJson(Map<String, dynamic> json) =>
+      _$ImportDogResultFromJson(json);
+  const ImportDogResult._();
+  int get length => dogs.length;
+
+  static ImportDogResult decode(String source) {
+    return ImportDogResult.fromJson(jsonDecode(source));
   }
 }
