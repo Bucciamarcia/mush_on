03e732d355129b0c8fe7755bf56107957e8f4fd7
Stefano Mini <info@stefanominiconsulting.com>
2026-04-18 16:59:01 +0300

    Adds import dogs logic to grid

---
+++ lib/kennel/import_dogs/riverpod.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'riverpod.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dogsToImportStateHash() => r'c1a034d40b321627e749d6307dbaf162b109a3b2';
+
+/// See also [DogsToImportState].
+@ProviderFor(DogsToImportState)
+final dogsToImportStateProvider =
+    AutoDisposeNotifierProvider<DogsToImportState, List<DogToImport>>.internal(
+      DogsToImportState.new,
+      name: r'dogsToImportStateProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$dogsToImportStateHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$DogsToImportState = AutoDisposeNotifier<List<DogToImport>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
